2056f35b2339bda4ad30f0b6d2d434ada6debe05
Selikovsky89 <askaraitov966@gmail.com>
2025-07-30 17:40:16 +0500

    бу

--- Cкрипты/chernovik.py
+++ Cкрипты/chernovik.py
@@ -1,9 +1,1 @@
-import ipaddress
-
-network = ipaddress.ip_network(f'63.178.184.0/255.255.255.192', 0)
-cnt = 0
-for ip in network:
-    b = '0' * (32 - len(bin(int(ip))[2:])) + bin(int(ip))[2:]
-    if b[:16].count('1') >= b[16:].count('1'):
-        cnt += 1
-print(cnt)
+print(len('QWERTYUIOPLKJHGFDSAZXCVBNM'))
